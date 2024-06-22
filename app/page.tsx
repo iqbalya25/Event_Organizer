d1d42996344818caea401e775b83724bc9da135c
Kurniawan Muhammad Rizki <32743784+KurniawanMuhammadRizki@users.noreply.github.com>
2024-06-22 19:28:30 +0700

    Feat(Component) add: Add footer (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Event from "./components/Event";
 import Expo from "./components/Expo";
 import FloatMenu from "./components/FloatingMenu";
+import Footer from "./components/footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -11,6 +12,7 @@ export default function Home() {
       <FloatMenu />
       <Hero />
       <Expo />
+      <Footer />
     </main>
   );
 }
