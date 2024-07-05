6f90b88f111787249dbc956b7f671fef337923ce
iqbalya25 <iqbalya25@gmail.com>
2024-07-05 16:58:11 +0700

    fixing bug drploy

--- app/components/Card/companyCard.tsx
+++ app/components/Card/companyCard.tsx
@@ -1,15 +1,15 @@
-import { partnersList } from "@/app/partner/partnerList";
+import { PartnersList } from "@/app/partner/partnerList";
 import React from "react";
 
 interface PartnerCardProps {
-  partnersItem: partnersList[];
+  partnersItem: typeof PartnersList;
 }
 
 const CompanyCard: React.FC<PartnerCardProps> = ({ partnersItem }) => {
   return (
     <div className="pt-24 pb-24 flex flex-col gap-10 border-b-2 border-black">
       {partnersItem.map((partner, index) => (
-        <div key={index} className=" border-b-2 border-gray-300">
+        <div key={index} className="border-b-2 border-gray-300">
           <div className="text-center p-5 flex flex-col justify-center gap-10 lg:flex-row lg:justify-normal bg-white">
             <div className="flex items-center">{partner.logo}</div>
             <div className="flex flex-col items-start gap-5">
