4e66366b8068992b279de1d893f50a3f22f53a07
iqbalya25 <iqbalya25@gmail.com>
2024-07-13 21:20:42 +0700

    fixing expo for momentary

--- app/expo/[eventDetailSlug]/page.tsx
+++ app/expo/[eventDetailSlug]/page.tsx
@@ -5,13 +5,11 @@ import EventOrganizerSection from "./eventOrganizerSection";
 import EventSpeakersSection from "./eventSpeakersSection";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/footer";
-import EventPage from "./[id]";
 
 const EventDetail: React.FC = () => {
   return (
     <main>
       <Navbar />
-      <EventPage />
       <EventSpeakersSection />
       <EventOrganizerSection />
       <Footer />
