1f4621e1579ffadb7f7fd672ab55a9038f2778c7
iqbalya25 <iqbalya25@gmail.com>
2024-07-14 21:37:10 +0700

    re deploy for eventslug detail fix

--- app/expo/[eventSlug]/EventSpeaker.tsx
+++ app/expo/[eventSlug]/EventSpeaker.tsx
@@ -11,7 +11,7 @@ const EventSpeakers: React.FC<EventSpeakersProps> = ({ speakers = [] }) => {
 
   return (
     <div>
-      <h2 className="text-3xl font-semibold mb-4">Speakers</h2>
+      <h2 className="text-3xl font-semibold mb-4">Our Speakers</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
         {speakers.map((speaker, index) => (
           <div key={index} className="text-center">
