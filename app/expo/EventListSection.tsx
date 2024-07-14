2e64924d802344c8d21d7deebd54bcf112863f9a
iqbalya25 <iqbalya25@gmail.com>
2024-07-15 01:27:45 +0700

    fixing tsx file naming

--- app/expo/EventListSection.tsx
+++ app/expo/EventListSection.tsx
@@ -8,7 +8,7 @@ const EventListSection: React.FC = () => {
     <div>
       <div className="py-10 px-16 flex flex-col items-center justify-center bg-black">
         <h1 className="text-2xl text-center md:text-4xl lg:text-6xl font-bold text-white">
-          Our Event & Exhibition
+          Our Event & Exhibitions
         </h1>
       </div>
       <div className="mx-auto px-16 pt-20 ">
