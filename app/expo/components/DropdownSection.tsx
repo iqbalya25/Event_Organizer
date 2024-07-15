a1b6c07ec9095f319343031c5c8abd5ef33f7aa1
iqbalya25 <iqbalya25@gmail.com>
2024-07-15 08:04:31 +0700

    adding filter dropdown box

--- app/expo/components/DropdownSection.tsx
+++ app/expo/components/DropdownSection.tsx
@@ -6,7 +6,9 @@ interface SortDropdownProps {
 }
 
 const SortDropdown: React.FC<SortDropdownProps> = ({ onSortChange }) => {
-  const [sortField, setSortField] = useState<"dateStart" | "name">("dateStart");
+  const [sortField, setSortField] = useState<
+    "dateStart" | "name" | "eventType"
+  >("dateStart");
   const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
 
   useEffect(() => {
@@ -14,17 +16,20 @@ const SortDropdown: React.FC<SortDropdownProps> = ({ onSortChange }) => {
   }, [sortField, sortDirection, onSortChange]);
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-row items-center justify-center space-x-4">
       <select
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         value={sortField}
-        onChange={(e) => setSortField(e.target.value as "dateStart" | "name")}
+        onChange={(e) =>
+          setSortField(e.target.value as "dateStart" | "name" | "eventType")
+        }
       >
         <option value="dateStart">Date</option>
         <option value="name">Alphabet</option>
+        <option value="eventType">Event Type</option>
       </select>
       <select
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         value={sortDirection}
         onChange={(e) => setSortDirection(e.target.value as "asc" | "desc")}
       >
