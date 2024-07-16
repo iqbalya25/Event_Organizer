0364a5c637577ec128e6715e7e943b070c0bc81a
iqballl <155224155+iqbalya25@users.noreply.github.com>
2024-07-16 14:18:52 +0700

    Add speaker feat (#28)
    * adding speaker feature
    
    * adding speakerForm

---
+++ app/user/eventorganizer/addfolder/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddSpeakerSection from "./AddSpeakerSection";
+
+const AddSpeakerPage = () => {
+  return (
+    <div>
+      <AddSpeakerPage />
+    </div>
+  );
+};
+
+export default AddSpeakerPage;
---
+++ app/user/eventorganizer/addspeaker/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddedSpeak from "./AddSpeakerPage";
+import AddSpeakerPage from "./AddSpeakerPage";
+
+const AddSpeaker = () => {
+  return (
+    <div>
+      <AddSpeakerPage />
+    </div>
+  );
+};
+
+export default AddSpeaker;
