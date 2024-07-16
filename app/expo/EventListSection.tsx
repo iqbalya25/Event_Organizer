bd171a2ef0f750b42217bba98690ad630d71fa19
iqbalya25 <iqbalya25@gmail.com>
2024-07-16 10:25:03 +0700

    fixing for deploy vercel

--- app/expo/EventListSection.tsx
+++ app/expo/EventListSection.tsx
@@ -9,7 +9,7 @@ const EventListSection: React.FC = () => {
     <div>
       <div className="py-10 px-16 flex flex-col items-center justify-center bg-black">
         <h1 className="text-2xl text-center md:text-4xl lg:text-6xl font-bold text-white">
-          Our Event & Exhibition
+          Our Event & Exhibitions
         </h1>
       </div>
       <EventCard />
