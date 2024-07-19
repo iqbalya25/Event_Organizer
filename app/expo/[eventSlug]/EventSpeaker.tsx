021f1b8460cb0b3426b9e3e2147f29d4c89ac455
iqbalya25 <iqbalya25@gmail.com>
2024-07-19 09:57:28 +0700

    fixing axios post for reservation

--- app/expo/[eventSlug]/EventSpeaker.tsx
+++ app/expo/[eventSlug]/EventSpeaker.tsx
@@ -10,24 +10,26 @@ const EventSpeakers: React.FC<EventSpeakersProps> = ({ speakers = [] }) => {
   if (!speakers.length) return null;
 
   return (
-    <div>
-      <h2 className="text-3xl font-semibold mb-4">Our Speakers</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="container mx-auto p-4">
+      <h2 className="text-3xl font-semibold mb-6 text-center">Our Speakers</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {speakers.map((speaker, index) => (
-          <div key={index} className="text-center">
+          <div
+            key={index}
+            className="bg-gray-200 shadow-lg rounded-lg overflow-hidden"
+          >
             <img
               src={speaker.profileImgUrl || "/placeholder-image.jpg"}
               alt={speaker.name}
-              className="w-full h-48 object-cover rounded-full mx-auto"
+              className="w-full h-48 object-cover"
             />
-            
-            <h3 className="text-xl font-semibold mt-2">{speaker.name}</h3>
-            <p className="text-gray-500">{speaker.position}</p>
+            <div className="p-4">
+              <h3 className="text-xl font-semibold mb-1">{speaker.name}</h3>
+              <p className="text-gray-500">{speaker.position}</p>
+            </div>
           </div>
         ))}
-    
       </div>
-      
     </div>
   );
 };
